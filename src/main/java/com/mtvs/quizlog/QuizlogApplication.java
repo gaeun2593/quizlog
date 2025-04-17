6c6cb2c6224a8019501dd6e3044719773b31f423
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-04-17 17:27:46 +0900

    Initial commit

---
+++ src/main/java/com/mtvs/quizlog/QuizlogApplication.java
@@ -0,0 +1,13 @@
+package com.mtvs.quizlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mtvs/quizlog/QuizlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtvs.quizlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
