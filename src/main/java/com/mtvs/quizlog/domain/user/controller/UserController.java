298237ef323bdd46488414f25de625e5aba26c9c
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-04-21 17:59:50 +0900

    feat: 닉네임 수정 기능 추가

--- src/main/java/com/mtvs/quizlog/domain/user/controller/UserController.java
+++ src/main/java/com/mtvs/quizlog/domain/user/controller/UserController.java
@@ -1,17 +1,16 @@
 package com.mtvs.quizlog.domain.user.controller;
 
 import com.mtvs.quizlog.domain.user.dto.request.SignUpRequestDTO;
+import com.mtvs.quizlog.domain.user.dto.request.UpdateNicknameRequestDTO;
 import com.mtvs.quizlog.domain.user.dto.response.SignUpResponseDTO;
+import com.mtvs.quizlog.domain.user.dto.response.UpdateNicknameResponseDTO;
 import com.mtvs.quizlog.domain.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -26,6 +25,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    // 회원가입
     @PostMapping("/sign-up")
     public ResponseEntity<SignUpResponseDTO> createUser(@Validated @RequestBody SignUpRequestDTO signUpRequestDTO) {
         log.info("createUser: {}", signUpRequestDTO.getNickname());
@@ -34,4 +34,43 @@ public class UserController {
 
         return ResponseEntity.ok().body(savedUser);
     }
+
+    // 로그인
+
+    // 로그아웃
+
+    // 닉네임 수정
+    @PatchMapping("/update-nickname/{userId}")
+    public ResponseEntity<UpdateNicknameResponseDTO> updateNickname(@PathVariable("userId") Long userId,
+            @Validated @RequestBody UpdateNicknameRequestDTO updateNicknameRequestDTO) {
+        log.info("petch : {}", userId);
+
+        UpdateNicknameResponseDTO updateNickname = userService.updateNickname(userId, updateNicknameRequestDTO);
+
+        if (updateNickname == null) {
+            return ResponseEntity.status(500).body(null);
+        } else {
+            return ResponseEntity.ok().body(updateNickname);
+        }
+    }
+
+    // 이메일 수정
+    @PatchMapping("/update-email/{userId}")
+    public ResponseEntity<UpdateNicknameResponseDTO> updateNickname(@PathVariable("userId") Long userId,
+                                                                    @Validated @RequestBody UpdateNicknameRequestDTO updateNicknameRequestDTO) {
+        log.info("petch : {}", userId);
+
+        UpdateNicknameResponseDTO updateNickname = userService.updateNickname(userId, updateNicknameRequestDTO);
+
+        if (updateNickname == null) {
+            return ResponseEntity.status(500).body(null);
+        } else {
+            return ResponseEntity.ok().body(updateNickname);
+        }
+    }
+
+    // 역할 수정
+    // 비밀번호 수정
+    // 회원 탈퇴
+
 }
--- src/main/java/com/mtvs/quizlog/domain/user/service/UserService.java
+++ src/main/java/com/mtvs/quizlog/domain/user/service/UserService.java
@@ -1,7 +1,9 @@
 package com.mtvs.quizlog.domain.user.service;
 
 import com.mtvs.quizlog.domain.user.dto.request.SignUpRequestDTO;
+import com.mtvs.quizlog.domain.user.dto.request.UpdateNicknameRequestDTO;
 import com.mtvs.quizlog.domain.user.dto.response.SignUpResponseDTO;
+import com.mtvs.quizlog.domain.user.dto.response.UpdateNicknameResponseDTO;
 import com.mtvs.quizlog.domain.user.entity.Status;
 import com.mtvs.quizlog.domain.user.entity.User;
 import com.mtvs.quizlog.domain.user.repository.UserRepository;
@@ -58,8 +60,37 @@ public class UserService {
                 .build();
 
         User savedUser = userRepository.save(user);
+        log.info("saved user: {}", savedUser);
 
         return new SignUpResponseDTO(savedUser.getNickname(), savedUser.getEmail(), savedUser.getPassword(),
                 savedUser.getRole(), savedUser.getStatus(), savedUser.getCreatedAt());
+    }
+
+    public UpdateNicknameResponseDTO updateNickname(Long userId, UpdateNicknameRequestDTO updateNicknameRequestDTO) {
+        log.info("updateNickname: {}", updateNicknameRequestDTO.getNickname());
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
+
+        // 닉네임 중복 검사
+        Optional<User> findUserNickname = userRepository.findByNickname(updateNicknameRequestDTO.getNickname());
+        if (findUserNickname.isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+        }
+
+        // 사용자 상태 확인
+        Optional<User> findUserStatus = userRepository.findById(userId);
+        if (findUserStatus.get().getStatus() != Status.ACTIVE) {
+            throw new IllegalArgumentException("탈퇴된 회원입니다.");
+        }
+
+        User updatedUser = user.toBuilder()
+                .nickname(updateNicknameRequestDTO.getNickname())
+                .updatedAt(LocalDate.now())
+                .build();
+
+        User savedUser = userRepository.save(updatedUser);
+        log.info("saved user: {}", savedUser);
+
+        return new UpdateNicknameResponseDTO(savedUser.getNickname(), savedUser.getUpdatedAt());
     }
 }
