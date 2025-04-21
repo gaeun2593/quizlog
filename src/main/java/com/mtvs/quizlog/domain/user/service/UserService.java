23f069662687fbe7553b8c6f8056f8a2b26caf56
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-04-21 16:15:27 +0900

    feat: 회원가입 기능 구현

--- src/main/java/com/mtvs/quizlog/domain/user/service/UserService.java
+++ src/main/java/com/mtvs/quizlog/domain/user/service/UserService.java
@@ -2,25 +2,64 @@ package com.mtvs.quizlog.domain.user.service;
 
 import com.mtvs.quizlog.domain.user.dto.request.SignUpRequestDTO;
 import com.mtvs.quizlog.domain.user.dto.response.SignUpResponseDTO;
+import com.mtvs.quizlog.domain.user.entity.Status;
+import com.mtvs.quizlog.domain.user.entity.User;
 import com.mtvs.quizlog.domain.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private static final Logger log = LoggerFactory.getLogger(UserService.class);
     private UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    @Transactional
     public SignUpResponseDTO createUser(SignUpRequestDTO signUpRequestDTO) {
+        log.info("createUser: {}", signUpRequestDTO.getNickname());
 
+        // 닉네임 중복 검사
+        Optional<User> findUserNickname = userRepository.findByNickname(signUpRequestDTO.getNickname());
+        if (findUserNickname.isPresent()) {
+            throw new IllegalArgumentException("nickname already exists");
+        }
+
+        // 이메일 중복 검사
+        Optional<User> findUserEmail = userRepository.findByEmail(signUpRequestDTO.getEmail());
+        if (findUserEmail.isPresent()) {
+            throw new IllegalArgumentException("email already exists");
+        }
+
+        // 비밀번호 확인 일치 여부 검사
+        if (!signUpRequestDTO.getPassword().equals(signUpRequestDTO.getPasswordCheck())) {
+            throw new IllegalArgumentException("password does not match");
+        }
+
+        User user = User.builder()
+                .nickname(signUpRequestDTO.getNickname())
+                .email(signUpRequestDTO.getEmail())
+                .password(bCryptPasswordEncoder.encode(signUpRequestDTO.getPassword()))
+                .role(signUpRequestDTO.getRole())
+                .status(Status.ACTIVE)
+                .createdAt(LocalDate.now())
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        return new SignUpResponseDTO(savedUser.getNickname(), savedUser.getEmail(), savedUser.getPassword(),
+                savedUser.getRole(), savedUser.getStatus(), savedUser.getCreatedAt());
     }
 }
