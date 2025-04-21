cc2f089b7e65d45bd8af3278559330d1ca6ac617
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-04-21 16:16:03 +0900

    chore: NotNull 어노테이션 추가

--- src/main/java/com/mtvs/quizlog/domain/user/dto/response/SignUpResponseDTO.java
+++ src/main/java/com/mtvs/quizlog/domain/user/dto/response/SignUpResponseDTO.java
@@ -6,25 +6,44 @@ import com.mtvs.quizlog.domain.user.entity.Status;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.time.LocalDate;
 
 @Data
 public class SignUpResponseDTO {
 
+    @NonNull
     private Long id;
 
+    @NonNull
     private String nickname;
+
+    @NonNull
     private String email;
+
+    @NonNull
     private String password;
 
     @Enumerated(EnumType.STRING)
+    @NonNull
     private Role role;
 
     @Enumerated(EnumType.STRING)
+    @NonNull
     private Status status;
 
+    @NonNull
     private LocalDate createdAt;
     private LocalDate updatedAt;
     private LocalDate deletedAt;
+
+    public SignUpResponseDTO(@NonNull String nickname, @NonNull String email, @NonNull String password, @NonNull Role role, @NonNull Status status, @NonNull LocalDate createdAt) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
 }
