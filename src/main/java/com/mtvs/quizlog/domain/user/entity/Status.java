11259f0e61c602db2e3b60b97ec7da21c0967ec4
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-04-21 14:02:30 +0900

    feat: User 엔티티 설정

---
+++ src/main/java/com/mtvs/quizlog/domain/user/entity/Status.java
@@ -0,0 +1,7 @@
+package com.mtvs.quizlog.domain.user.entity;
+
+public enum Status {
+
+    ACTIVE,
+    DELETED
+}
--- src/main/java/com/mtvs/quizlog/domain/user/entity/User.java
+++ src/main/java/com/mtvs/quizlog/domain/user/entity/User.java
@@ -1,4 +1,44 @@
 package com.mtvs.quizlog.domain.user.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String nickname;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+    private LocalDate deletedAt;
+
+    protected User() {}
+
+    public User(String nickname, String email, String password, Role role, Status status, LocalDate createdAt, LocalDate updatedAt, LocalDate deletedAt) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
 }
