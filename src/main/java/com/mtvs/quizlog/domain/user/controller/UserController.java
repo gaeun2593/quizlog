456de5a34e197537974e2202c2d975e7dfba80ea
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-04-23 13:01:09 +0900

    chore: 어노테이션 변경

--- src/main/java/com/mtvs/quizlog/domain/user/controller/UserController.java
+++ src/main/java/com/mtvs/quizlog/domain/user/controller/UserController.java
@@ -7,10 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 @Validated
 public class UserController {
@@ -32,10 +33,6 @@ public class UserController {
 
         return ResponseEntity.ok().body(savedUser);
     }
-
-    // 로그인
-
-    // 로그아웃
 
     // 닉네임 수정
     @PatchMapping("/update-nickname/{userId}")
