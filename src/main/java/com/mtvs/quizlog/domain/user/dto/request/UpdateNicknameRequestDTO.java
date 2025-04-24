fc34fb367ee758b85616382a4914ecc5c8783db9
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-04-24 09:47:12 +0900

    feat: 마이페이지 추가

--- src/main/java/com/mtvs/quizlog/domain/user/dto/request/UpdateNicknameRequestDTO.java
+++ src/main/java/com/mtvs/quizlog/domain/user/dto/request/UpdateNicknameRequestDTO.java
@@ -3,11 +3,19 @@ package com.mtvs.quizlog.domain.user.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UpdateNicknameRequestDTO {
 
     @NotBlank(message = "닉네임은 필수 입력값입니다.")
     @Size(min = 2, max = 15, message = "닉네임은 2글자 이상 15글자 이하로 입력해주세요.")
     private String nickname;
+
+    public UpdateNicknameRequestDTO() { }
+
+    public UpdateNicknameRequestDTO(String nickname) {
+        this.nickname = nickname;
+    }
 }
