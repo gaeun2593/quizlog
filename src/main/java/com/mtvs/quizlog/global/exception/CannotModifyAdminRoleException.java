6981e026e1568109a83e48125041ff2f434b194e
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-04-27 23:27:28 +0900

    remove: 예외처리 파일 삭제

--- src/main/java/com/mtvs/quizlog/global/exception/CannotModifyAdminRoleException.java
+++
@@ -1,7 +0,0 @@
-package com.mtvs.quizlog.global.exception;
-
-public class CannotModifyAdminRoleException extends RuntimeException {
-    public CannotModifyAdminRoleException(String message) {
-        super(message);
-    }
-}
